91c4eefa4a0c8a37e08b5577342f2a7c2def86d4
Martín Szyszlican <contacto@martinszyszlican.com>
2022-06-30 00:21:18 -0500

    Start the login server

---
+++ login_server/routes/users.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+
+var passport = require('passport');
+var Account = require('../models/account');
+
+module.exports = function (app) {
+
+  router.get('/', function (req, res, next) {
+      res.render('index', { user : req.user });
+  });
+
+  router.get('/register', function(req, res, next) {
+      res.render('register', { });
+  });
+
+  router.post('/register', function(req, res, next) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+          return res.render("register", {info: "Sorry. That username already exists. Try again."});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+          res.redirect('/');
+        });
+    });
+});
+
+
+  router.get('/login', function(req, res, next) {
+      res.render('login', { user : req.user });
+  });
+
+  router.post('/login', passport.authenticate('local'), function(req, res, next) {
+      res.redirect('/');
+  });
+
+  router.get('/logout', function(req, res, next) {
+      req.logout();
+      res.redirect('/');
+  });
+
+  router.get('/ping', function(req, res){
+      res.send("pong!", 200);
+  });
+
+};
+
